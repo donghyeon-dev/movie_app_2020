925f35faa032e1ee4fda119ab03416db13d1db30
donghyeon-dev <donghaynzxc@gmail.com>
2020-01-29 13:00:36 +0900

    Sharing props between Routes

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,7 +1,13 @@
 import React from "react";
 
-function About() {
-  return <span>About this page : I built it because i love movies.</span>;
+function About(props) {
+  console.log(props);
+  return (
+    <div className="about__container">
+      <span>"나는 뭐라고 써야할지 모르겠지만 일단 써본다"</span>;
+      <span>박동현, 1994</span>
+    </div>
+  );
 }
 
 export default About;
